3d5c38267dd7606b83caca286595babfc7a92631
StephanoWurttele <stephano.wurttele@utec.edu.pe>
2020-10-11 16:28:37 -0500

    thread logic

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-
 #include <iostream>
+#include <pthread.h>
 #include <string>
 #include <fstream>
 #include <vector>
@@ -10,55 +10,85 @@
 
 using namespace std;
 
-struct Register{
-    char name[30];
-    char user[30];
-    char mail[41];
-    char pass[12];
 
-    Register(){};
+short get_operation(const char op[10]){
+  short operation = 3;
+  if (!strcmp(op, "SELECT")) operation = 0;
+  else if (!strcmp(op, "INSERT")) operation = 1;
+  else if (!strcmp(op, "DELETE")) operation = 2;
+  return operation;
+}
 
-    Register(string nam, string usr, string ml, string pss){
-        nam = nam.substr(0,29);
-        usr = usr.substr(0,29);
-        ml = ml.substr(0,40); 
-        pss = pss.substr(0,11);      
-
-        strcpy(name, (nam + string(30 - nam.length() ,' ')).c_str());
-        strcpy(user, (usr + string(30 - usr.length() ,' ')).c_str());
-        strcpy(mail, (ml + string(41 - ml.length() ,' ')).c_str());
-        strcpy(pass, (pss + string(12 - pss.length() ,' ')).c_str());
+struct Query{
+  short operation;
+  string key;
+  //struct Register reg;
+  //Query(const char op[10], string key[30]){
+  //};
+  Query(string query1){
+    stringstream query(query1);
+    string q1;
+    getline(query, q1, ' ');
+    cout << "Query type: " << q1 << endl;
+    operation = get_operation(q1.c_str());
+    string strs[5];
+    int temp = 0;
+    switch(operation){
+      case 0:
+      case 2:
+        cout << "0 or 1" << endl;
+        while(q1 != "="){
+          getline(query, q1, ' ');
+        }
+        getline(query, q1, ' ');
+        q1 = q1.substr(1, q1.length()-3);
+        cout << q1 << endl;
+        break;
+      case 1:
+        while(q1 != "VALUES"){
+          getline(query, q1, ' ');
+        }
+        cout << "Q1 ------: " << q1 << endl;
+        getline(query, q1, ',');
+        do{
+          ++temp;
+          cout << "char: " << q1[temp] << " ";
+        }while(q1[temp] != 39);
+        getline(query, q1, ' ');
+        strs[0] = q1.substr(2, temp);
+        getline(query, q1, ' ');
+        for(int i = 1; i < 4; ++i){
+        temp = 0;
+        do{
+          ++temp;
+        }while(q1[temp] != 39);
+        strs[i] = q1.substr(1, temp-1);
+        getline(query, q1, ' ');
+        }
+        for(int i = 0; i < 4; ++i){
+          cout << strs[i] << endl;
+        }
+        break;
+      default:
+        cout << "Invalid query" << endl;
+        throw;
     }
+  }
 };
 
-vector<Register> registers;
+void execute_query(){
 
-void load_data(){
-	std::ifstream Usuario("Usuario.csv");
-	
-	string current;	
-	while(getline(Usuario, current)){
-        string name, user, mail, pass; 
-		stringstream ss(current);
-
-		getline(ss, current, ',');
-		name = current;
-		getline(ss, current, ',');
-        user = current;
-        getline(ss, current, ',');
-        mail = current;
-        getline(ss, current, ',');
-        pass = current;
-
-        Register temp(name, user, mail, pass);
-		registers.push_back(temp);
-	}
 }
 
 
-
 int main(){
-    load_data();
-    cout<<"done!\n";
-    return 0;
+  string query1("SELECT * FROM ISAM WHERE NOMBRE = 'Stephano';");
+  string query2("INSERT INTO ISAM VALUES ('Stephano Wurt', 'Stephano_Wurt', 'stephano_wurt@mail.com', 'abcdefghijkl');");
+  string query3("DELETE FROM ISAM WHERE NOMBRE = 'Stephano';");
+  string query4("Ay lmao");
+  //Query q1(query1);
+  Query q2(query2);
+  //Query q3(query3);
+  //Query q4(query4);
+  return 0;
 }
